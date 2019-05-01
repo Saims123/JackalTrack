e66c23cd6b59adb81e5487dd3fa812874d67d003
Steve Sim <stevesim@hotmail.com>
2019-05-01 16:25:45 +0100

    Minor : Usability to shift action buttons to the right

--- src/app/components/student/dialogbox/add-student-confirm.component.ts
+++ src/app/components/student/dialogbox/add-student-confirm.component.ts
@@ -3,12 +3,19 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 @Component({
   selector: 'add-confirmation-dialog',
   templateUrl: 'add-student-confirmation.html',
-  styles: []
+  styles: [
+    `
+      .mat-dialog-actions{
+        float: right;
+      }
+    `
+  ]
 })
 export class AddStudentConfirmationComponent {
   constructor(
     public dialogRef: MatDialogRef<AddStudentConfirmationComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
